9d0d6fc947bac2cc79b5a8c541340a7cba6b51d6
Benoit GRASSET <b.grasset@cliqdigital.com>
2023-12-19 19:17:32 +0100

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,90 @@
 import { useState } from "react";
 import "./App.css";
 
+const labelStyle = { margin: "0 5px" };
+const inputStyle = { width: "50px" };
+
+const todayDate = new Date().toLocaleDateString().replaceAll("/", "-");
+
 function App() {
-  const [fields, setFields] = useState([{ duration: 0, power: 0, pace: 0 }]);
+  const [fields, setFields] = useState([{ duration: 0, power: 0, pace: 80 }]);
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const newFields = [...fields].map((field) => {
+      return {
+        duration: field.duration * 60,
+        power: Math.round((field.power / 316) * 10) / 10,
+        pace: field.pace,
+      };
+    });
+    const xmlString = `
+      <workout_file>
+        <author/>
+        <name>New-Workout-${todayDate}</name>
+        <description/>
+        <sportType>bike</sportType>
+        <durationType>time</durationType>
+        <tags/>
+        <workout>
+        ${newFields.map((field) => {
+          return `<SteadyState Duration="${field.duration}" Power="${field.power}" pace="${field.pace}"/> \n`;
+        })}
+        </workout>
+        </workout_file>
+`;
+    console.log(xmlString);
+  };
 
   return (
-    <div>
+    <form noValidate onSubmit={handleSubmit}>
       <h1>Zwift ZWO Editor</h1>
-      {fields.map((field) => (
-        <div>
+      {fields.map((field, index) => (
+        <div style={{ margin: "5px" }}>
           <span>
-            <label>Duration</label>
+            <label style={labelStyle} htmlFor={`duration-${index}`}>
+              Duration (min)
+            </label>
             <input
+              id={`duration-${index}`}
+              style={inputStyle}
               type="text"
               value={field.duration}
               onChange={(e) => {
                 const newFields = [...fields];
-                newFields[0].duration = parseInt(e.target.value);
+                newFields[index].duration = parseInt(e.target.value);
                 setFields(newFields);
               }}
             />
           </span>
           <span>
-            <label>Power</label>
+            <label style={labelStyle} htmlFor={`power-${index}`}>
+              Power (Watts)
+            </label>
             <input
+              id={`power-${index}`}
+              style={inputStyle}
               type="text"
               value={field.power}
               onChange={(e) => {
                 const newFields = [...fields];
-                newFields[0].power = parseInt(e.target.value);
+                newFields[index].power = parseInt(e.target.value);
                 setFields(newFields);
               }}
             />
           </span>
           <span>
-            <label>Pace</label>
+            <label style={labelStyle} htmlFor={`pace-${index}`}>
+              Pace (RPM)
+            </label>
             <input
+              id={`pace-${index}`}
+              style={inputStyle}
               type="text"
               value={field.pace}
               onChange={(e) => {
                 const newFields = [...fields];
-                newFields[0].pace = parseInt(e.target.value);
+                newFields[index].pace = parseInt(e.target.value);
                 setFields(newFields);
               }}
             />
@@ -48,13 +92,19 @@ function App() {
         </div>
       ))}
       <button
+        type="button"
         onClick={() =>
           setFields([...fields, { duration: 0, power: 0, pace: 0 }])
         }
       >
         Add
       </button>
-    </div>
+      <input
+        type="submit"
+        value="Submit"
+        style={{ display: "block", width: "100%", marginTop: "10px" }}
+      />
+    </form>
   );
 }
 
