b22667fda49d3d3e34c417f4c4a82702f24a7a20
Benoit GRASSET <b.grasset@cliqdigital.com>
2024-01-03 13:38:52 +0100

    fix: wip

--- src/App.tsx
+++ src/App.tsx
@@ -15,42 +15,11 @@ import { MdAdd, MdDownload } from "react-icons/md";
 import "./App.css";
 import { styles } from "./index.styles";
 import { createXMLString, downLoadFile } from "./utils";
-
-const colorsByPower = {
-  z1: "#767676", // grey - recovery
-  z2: "#01b2cc", // blue - endurance
-  z3: "#5bbe5b", // green - tempo
-  z4: "#fbcc42", // yellow - threshold
-  z5: "#fb6418", // orange - vo2max
-  z6: "#ff0000", // red - anaerobic
-};
-
-const lightColorsByPower = {
-  z1: "#c8c8c8", // grey - recovery
-  z2: "#86effe", // blue - endurance
-  z3: "#bde5bd", // green - tempo
-  z4: "#fdebb3", // yellow - threshold
-  z5: "#fdc1a3", // orange - vo2max
-  z6: "#ff9999", // red - anaerobic
-};
-
-const getPowerPercentColor = (powerPercent: number) => {
-  if (powerPercent < 0.6) return colorsByPower.z1;
-  if (powerPercent < 0.75) return colorsByPower.z2;
-  if (powerPercent < 0.89) return colorsByPower.z3;
-  if (powerPercent < 1.04) return colorsByPower.z4;
-  if (powerPercent < 1.18) return colorsByPower.z5;
-  return colorsByPower.z6;
-};
-
-const getPowerPercentLightColor = (powerPercent: number) => {
-  if (powerPercent < 0.6) return lightColorsByPower.z1;
-  if (powerPercent < 0.75) return lightColorsByPower.z2;
-  if (powerPercent < 0.89) return lightColorsByPower.z3;
-  if (powerPercent < 1.04) return lightColorsByPower.z4;
-  if (powerPercent < 1.18) return lightColorsByPower.z5;
-  return lightColorsByPower.z6;
-};
+import {
+  colorsByPower,
+  getPowerPercentColor,
+  getPowerPercentLightColor,
+} from "./utils/colors";
 
 export type FinalField = {
   duration: number;
